0234cbb0cb0f797776248fd853817978b18810f4
s4ur4b <saurabhchavn2110@gmail.com>
2021-09-07 07:11:44 +0530

    Added: Nodes to the BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -54,6 +54,16 @@ public class BinarySearchTree {
         binary.insert(56);
         binary.insert(30);
         binary.insert(70);
+        binary.insert(22);
+        binary.insert(40);
+        binary.insert(11);
+        binary.insert(3);
+        binary.insert(16);
+        binary.insert(60);
+        binary.insert(95);
+        binary.insert(65);
+        binary.insert(63);
+        binary.insert(67);
 
         binary.inorderTraversal(binary.root);
     }
